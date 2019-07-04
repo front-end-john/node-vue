9a9771f3de900d65860709f8e10f2cae7efa217d
unknown <SKY225612@skyworth.com>
2019-07-04 17:14:54 +0800

    update vue.config
    Change-Id: I8f144e647e314c4962696a86e1a40022da9b36e8

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Node+Vue实战项目
+Node+Vue实战项目参考链接：
 https://blog.csdn.net/qq_25479327/article/details/81302296
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,35 @@
 module.exports = {
-  lintOnSave: false
+    publicPath: '/', // 配置路由基础路径， history 模式需使用绝对路径
+    outputDir: 'dist',
+    lintOnSave: true,
+    productionSourceMap: false,
+    configureWebpack: { // webpack 配置
+        output: { // 输出重构  打包编译后的 文件名称
+            filename: `js/[name].js?${Math.random()}`,
+            chunkFilename: `js/[name].js?${Math.random()}`
+        }
+    },
+    css: {
+        // 是否使用css分离插件 ExtractTextPlugin
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        loaderOptions: {}
+    },
+    devServer: {
+        open: true,
+        host: '0.0.0.0',
+        port: 10086,
+        https: false,
+        hotOnly: false,
+        proxy: null,
+        // proxy: {
+        //     '/api': {
+        //         target: '<url>',
+        //         ws: true,
+        //         changOrigin: true
+        //     }
+        // },
+        before: app => {}
+    }
 }
